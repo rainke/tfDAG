9f567e0b61bb96f716cb76c7847e1f4669afd050
hepeng <hepeng@gridsum.com>
2018-12-04 20:03:53 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Dag />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Dag from './components/Dag.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Dag,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="less">
+html, body  {
+  margin: 0;
+  height: 100%;
+}
 #app {
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  .overlay {
+    cursor: unset;
+  }
+  .node {
+    &:hover {
+      .linkpoint {
+        fill:aqua;
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/Dag.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="dag">
+    <div class="tool"></div>
+    <div class="design">
+      <div class="short"></div>
+      <div class="draw" ref="draw">
+        <div class="flow">
+          <svg width="1600" height="1600"></svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Emit} from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class Dag extends Vue {
+}
+
+</script>
+
+<style lang="less" scoped>
+@import '~@/style/variable.less';
+
+#dag {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .tool {
+    height: 30px;
+    background-color: aliceblue;
+  }
+  .design {
+    display: flex;
+    flex: 1;
+    overflow: hidden;
+    .short {
+      width: 120px;
+      background-color: beige;
+    }
+    .draw {
+      &::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+        background-color: lightblue;
+      }
+  
+      &::-webkit-scrollbar-thumb {
+        border-radius: 4px;
+        background-color: grey;
+      }
+  
+      &::-webkit-scrollbar-button {
+        width: 0;
+        height: 0;
+      }
+  
+      &::-webkit-scrollbar-track {
+        border-radius: 4px;
+      }
+      flex: 1;
+      overflow: scroll;
+
+      .flow {
+        width: 2000px;
+        height: 2000px;
+        background-color:@dga-marginclolr;
+        display: flex;
+        svg {
+          background: #fff;
+          background-image: 
+            repeating-linear-gradient(@dga-gridcolor, @dga-gridcolor 1%, transparent 0,  transparent 20%),
+            linear-gradient(transparent 99%, @dga-gridcolor 0),
+            repeating-linear-gradient(to right, @dga-gridcolor, @dga-gridcolor 1%, transparent 0,  transparent 20%),
+            linear-gradient(to right, transparent 99%, @dga-gridcolor 0);
+          background-size: 100px 100px;
+          margin: 40px 0 0 40px;
+        }
+      }
+    }
+  }
+}
+</style>
